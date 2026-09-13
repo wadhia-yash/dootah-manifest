24535fa80d0231dc6fe35ca659b01c905792aa08
wadhia-yash <aarushwadhia@gmail.com>
2026-09-13 18:37:04 +0530

    Cahier bundle 16: selected brush background is a rectangle

--- bundle.js
+++ bundle.js
@@ -4022,7 +4022,7 @@ if (typeof Math.clz32 === 'undefined') {
     // Inline function 'kotlin.collections.buildListInternal' call
     // Inline function 'kotlin.apply' call
     var this_2 = ArrayList_init_$Create$();
-    this_2.f1(new ComponentNode('androidx.compose.material3.Icon(contentDescription|modifier|painter|tint)', mapOf_0([to('painter', new PainterResourceProp('drawable:brush_24px')), to('contentDescription', new StringResourceProp('string:brush')), to('modifier', new ModifierProp(listOf(new ModifierOpNode('background', mapOf_0([to('color', isEraserMode ? new ColorProp(new Long(0, 0)) : new ThemeColorProp('inversePrimary')), to('shape', new ShapeProp('circle'))])))))])));
+    this_2.f1(new ComponentNode('androidx.compose.material3.Icon(contentDescription|modifier|painter|tint)', mapOf_0([to('painter', new PainterResourceProp('drawable:brush_24px')), to('contentDescription', new StringResourceProp('string:brush')), to('modifier', new ModifierProp(listOf(new ModifierOpNode('background', mapOf_0([to('color', isEraserMode ? new ColorProp(new Long(0, 0)) : new ThemeColorProp('inversePrimary')), to('shape', new ShapeProp('rectangle'))])))))])));
     var tmp$ret$8 = this_2.a2();
     this_1.f1(new ComponentNode('androidx.compose.material3.IconButton(colors|content|enabled|interactionSource|modifier|onClick|shape)', tmp_0, mapOf(to('content', tmp$ret$8))));
     this_1.f1(new ComponentNode('com.example.cahier.features.drawing.BrushesDropdownMenu(customBrushes|expanded|modifier|onBrushChange|onDismissRequest)', mapOf_0([to('expanded', new StateProp('brushesMenuExpanded')), to('onDismissRequest', new CallbackProp('brushesMenuExpanded.value=false', 0)), to('onBrushChange', new CallbackProp('drawingCanvasViewModel.changeBrush($0);brushesMenuExpanded.value=false', 1)), to('customBrushes', new HandleProp('customBrushes'))])));
@@ -4036,30 +4036,22 @@ if (typeof Math.clz32 === 'undefined') {
     this_3.f1(new ComponentNode('androidx.compose.material3.Icon(contentDescription|modifier|painter|tint)', mapOf_0([to('painter', new PainterResourceProp('drawable:palette_24px')), to('contentDescription', new StringResourceProp('string:color'))])));
     var tmp$ret$12 = this_3.a2();
     this_0.f1(new ComponentNode('androidx.compose.material3.IconButton(colors|content|enabled|interactionSource|modifier|onClick|shape)', tmp_1, mapOf(to('content', tmp$ret$12))));
-    var tmp_2 = mapOf_0([to('onClick', new CallbackProp('onColorPickerClick();drawingCanvasViewModel.setEraserMode(false)', 0)), to('modifier', new ModifierProp(listOf(new ModifierOpNode('size', mapOf_0([to('width', new DpProp(48.0)), to('height', new DpProp(48.0))])))))]);
+    var tmp_2 = emptyList();
     // Inline function 'kotlin.collections.buildList' call
     // Inline function 'kotlin.collections.buildListInternal' call
     // Inline function 'kotlin.apply' call
     var this_4 = ArrayList_init_$Create$();
-    this_4.f1(new ComponentNode('androidx.compose.material3.Icon(contentDescription|modifier|painter|tint)', mapOf_0([to('painter', new PainterResourceProp('drawable:palette_24px')), to('contentDescription', new StringResourceProp('string:color'))])));
-    var tmp$ret$16 = this_4.a2();
-    this_0.f1(new ComponentNode('androidx.compose.material3.IconButton(colors|content|enabled|interactionSource|modifier|onClick|shape)', tmp_2, mapOf(to('content', tmp$ret$16))));
-    var tmp_3 = emptyList();
+    var tmp_3 = mapOf_0([to('onClick', new CallbackProp('sizeMenuExpanded.value=true', 0)), to('modifier', new ModifierProp(listOf(new ModifierOpNode('size', mapOf_0([to('width', new DpProp(48.0)), to('height', new DpProp(48.0))])))))]);
     // Inline function 'kotlin.collections.buildList' call
     // Inline function 'kotlin.collections.buildListInternal' call
     // Inline function 'kotlin.apply' call
     var this_5 = ArrayList_init_$Create$();
-    var tmp_4 = mapOf_0([to('onClick', new CallbackProp('sizeMenuExpanded.value=true', 0)), to('modifier', new ModifierProp(listOf(new ModifierOpNode('size', mapOf_0([to('width', new DpProp(48.0)), to('height', new DpProp(48.0))])))))]);
-    // Inline function 'kotlin.collections.buildList' call
-    // Inline function 'kotlin.collections.buildListInternal' call
-    // Inline function 'kotlin.apply' call
-    var this_6 = ArrayList_init_$Create$();
-    this_6.f1(new ComponentNode('androidx.compose.material3.Icon(contentDescription|modifier|painter|tint)', mapOf_0([to('painter', new PainterResourceProp('drawable:line_weight_24px')), to('contentDescription', new StringResourceProp('string:brush_size'))])));
-    var tmp$ret$24 = this_6.a2();
-    this_5.f1(new ComponentNode('androidx.compose.material3.IconButton(colors|content|enabled|interactionSource|modifier|onClick|shape)', tmp_4, mapOf(to('content', tmp$ret$24))));
-    this_5.f1(new ComponentNode('com.example.cahier.features.drawing.SizeDropdownMenu(expanded|modifier|onDismissRequest|onSizeChange)', mapOf_0([to('expanded', new StateProp('sizeMenuExpanded')), to('onDismissRequest', new CallbackProp('sizeMenuExpanded.value=false', 0)), to('onSizeChange', new CallbackProp('drawingCanvasViewModel.changeBrushSize($0);sizeMenuExpanded.value=false', 1))])));
+    this_5.f1(new ComponentNode('androidx.compose.material3.Icon(contentDescription|modifier|painter|tint)', mapOf_0([to('painter', new PainterResourceProp('drawable:line_weight_24px')), to('contentDescription', new StringResourceProp('string:brush_size'))])));
     var tmp$ret$20 = this_5.a2();
-    this_0.f1(new BoxNode(tmp_3, tmp$ret$20));
+    this_4.f1(new ComponentNode('androidx.compose.material3.IconButton(colors|content|enabled|interactionSource|modifier|onClick|shape)', tmp_3, mapOf(to('content', tmp$ret$20))));
+    this_4.f1(new ComponentNode('com.example.cahier.features.drawing.SizeDropdownMenu(expanded|modifier|onDismissRequest|onSizeChange)', mapOf_0([to('expanded', new StateProp('sizeMenuExpanded')), to('onDismissRequest', new CallbackProp('sizeMenuExpanded.value=false', 0)), to('onSizeChange', new CallbackProp('drawingCanvasViewModel.changeBrushSize($0);sizeMenuExpanded.value=false', 1))])));
+    var tmp$ret$16 = this_4.a2();
+    this_0.f1(new BoxNode(tmp_2, tmp$ret$16));
     var tmp$ret$0 = this_0.a2();
     return new FragmentNode(tmp$ret$0);
   };
